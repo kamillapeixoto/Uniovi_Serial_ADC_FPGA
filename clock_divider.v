96779acdf0a9a3f9ccdf9d3c1f604592d7af85e1
kamillapeixoto <kamillap.peixoto@gmail.com>
2023-01-18 19:32:48 +0100

    Create clock_divider.v

---
+++ clock_divider.v
@@ -0,0 +1,56 @@
+// Quartus II Verilog Template
+// Binary counter
+
+module clock_divider
+(
+	clk_in, enable, reset, freq_userA, freq_userB, // dos bits para leer los interruptores de seleccion de frecuencia
+	
+	clk_out
+);
+
+
+	input clk_in, enable, reset, freq_userA, freq_userB;// dos bits para leer los interruptores de seleccion de frecuencia
+	output reg clk_out;
+	
+	integer freq_div; // Por cuanto dividiremos la frecuencia original de 50M Hz
+	integer count;    // Contador de flancos del reloj
+	wire [1:0] freq_user;
+	assign freq_user = {freq_userA, freq_userB};
+	
+	
+	// Si ha un cambio en los interruptores
+	always @(posedge clk_in)
+	begin
+	
+	
+			case (freq_user)
+				2'b00: freq_div = 256;
+				2'b01: freq_div = 128;
+				2'b10: freq_div = 64;	
+				2'b11: freq_div = 32;
+			endcase
+	end
+
+	// Reset if needed, or increment if counting is enabled
+	always @ (posedge clk_in or posedge reset)
+	begin
+		
+		if (reset)
+		begin
+			count = 0;
+			clk_out <= 0;
+		end
+		else if (enable)
+		begin
+			count = count + 1;
+			
+			if (count == freq_div)
+			begin
+				count = 0;
+				clk_out <= ~clk_out;
+			end
+		end // if (enable == 1'b1)
+	end
+
+endmodule
+
